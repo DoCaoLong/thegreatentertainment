29bd72f22ee128d954cccbe32c438a5092e78396
Đỗ Cao Long <long@myspa.vn>
2024-10-09 11:01:23 +0700

    [UPDATE] optimize

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 addEventListener("load", (event) => {
     // optimize view mobile
     if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-        // Mobile device style: fill the whole browser client area with the game canvas:
-
         var meta = document.createElement("meta");
         meta.name = "viewport";
         meta.content =
@@ -24,44 +22,39 @@ addEventListener("load", (event) => {
     // back to top
     let scrollTopBtn = document.getElementById("scrollTopBtn");
 
-    window.onscroll = function () {
-        scrollFunction();
-    };
+    if (scrollTopBtn) { // Kiểm tra nếu nút scroll top tồn tại
+        window.onscroll = function () {
+            scrollFunction();
+        };
 
-    function scrollFunction() {
-        if (
-            document.body.scrollTop > 20 ||
-            document.documentElement.scrollTop > 20
-        ) {
-            scrollTopBtn.style.display = "block";
-        } else {
-            scrollTopBtn.style.display = "none";
+        function scrollFunction() {
+            if (
+                document.body.scrollTop > 20 ||
+                document.documentElement.scrollTop > 20
+            ) {
+                scrollTopBtn.style.display = "block";
+            } else {
+                scrollTopBtn.style.display = "none";
+            }
         }
-    }
 
-    scrollTopBtn.onclick = function () {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
+        scrollTopBtn.onclick = function () {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        };
+    }
     // back to top
 
 });
 
 // avtive menu 
 document.addEventListener("DOMContentLoaded", function() {
-    // Lấy tất cả các link trong nav
     const navLinks = document.querySelectorAll('.nav-links a');
-
-    // Lấy đường dẫn hiện tại
     const currentPath = window.location.pathname;
 
-    // Duyệt qua tất cả các link và kiểm tra nếu trùng với đường dẫn hiện tại
     navLinks.forEach(link => {
-        console.log(link.getAttribute('href'))
+        console.log(link.getAttribute('href'));
         if (link.getAttribute('href') === currentPath) {
-            // Xóa lớp 'active' nếu có
             navLinks.forEach(link => link.classList.remove('active-menu'));
-            
-            // Thêm lớp 'active' vào link tương ứng
             link.classList.add('active-menu');
         }
     });
